2b6e3508fec9eac475d252c9ee312e6957e26864
Dulmaa-t <duka9705@gmail.com>
2022-12-14 15:43:44 +0800

    Update Register.tsx

--- src/views/Register.tsx
+++ src/views/Register.tsx
@@ -12,18 +12,24 @@ const RegisterPage: FunctionComponent<RegisterPageProps> = (): JSX.Element => {
         <div className="w-full pt-8 justify-center items-center flex flex-col container mx-auto">
             <form className="gap-4 p-8 w-[420px]">
                 <Text className="text-center text-[24px] mb-4">Log In</Text>
-                <div className="flex flex-col items-center">
+                <div className="flex flex-col">
                     <div className="flex p-4 h-20 w-full">
                         <Input id="loginId" placeholder="Username" />
                     </div>
                     <div className="flex p-4 h-20 w-full">
                         <Input id="password" placeholder="Password" />
                     </div>
-                    <div className="flex w-8 h-8">
-                        <Input id="remember" type="checkbox"/>
+                    <div className="flex flex-row w-8 h-8 px-6">
+                        <div className="flex mr-2">
+                            <input className="" id="remember" type="checkbox" />
+                        </div>
+                        <div className="flex items-center">
+                            <p className="mr-1 text-[#999]">Save</p>
+                            <p className="text-[#999]">ID</p>
+                        </div>
                     </div>
-                    <div className="flex">
-                        <button className="bg-black px-8 py-4 text-white">confirm</button>
+                    <div className="flex w-full px-4 py-4">
+                        <button className="bg-black px-8 py-4 text-white w-full rounded-md">confirm</button>
                     </div>
                 </div>
             </form>
